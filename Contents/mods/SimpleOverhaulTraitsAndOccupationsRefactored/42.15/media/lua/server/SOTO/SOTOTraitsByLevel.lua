4891ee061fee903836193d384301682675b24bbd
Susjin <pedrohgmoreira@gmail.com>
2026-09-25 14:48:41 -0300

    feat: finish all traits gained by level

--- Contents/mods/SimpleOverhaulTraitsAndOccupationsRefactored/42.15/media/lua/server/SOTO/SOTOTraitsByLevel.lua
+++ Contents/mods/SimpleOverhaulTraitsAndOccupationsRefactored/42.15/media/lua/server/SOTO/SOTOTraitsByLevel.lua
@@ -9,11 +9,28 @@
 --- @class SOTOTraitsByLevel
 local SOTOTraitsByLevel = {}
 ----------------------------------------------------------------------------------------------
+--Requires
+local SOTOUtility = require("SOTO/SOTOUtility")
+local SOTOTraits = require("SOTO/SOTOTraits")
+
+--Pulling global to local for performance
+local SOTOSandbox = SandboxVars.SOTO
 
 
+---Executed every time a player gets a level up on a skill
+---@param player IsoPlayer
+---@param perk PerkFactory.Perk
+---@param perkLevel number
+function SOTOTraitsByLevel.main(player, perk, perkLevel)
+    local perkType = perk:getType()
+    if SOTOTraits[perkType] then
+        SOTOTraits[perkType](player, perk, perkLevel)
+    else
+        SOTOUtility.logDebug(string.format("Function for perk %s not found", perkType), SOTOTraits.ByLevel.location, SOTOUtility.getGameMode())
+    end
+end
 
-
-
+Events.LevelPerk.Add(SOTOTraitsByLevel.main)
 
 ------------------ Returning file for 'require' ------------------
 return SOTOTraitsByLevel
